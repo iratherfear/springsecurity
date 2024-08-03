585ddcaf61bf26b5017e5ecfef5c5092d4e01c13
iratherfear <irather@fear.mail>
2024-08-03 17:32:17 +0530

    Added JDBC  datasource with password encoder

--- src/main/java/dev/iratherfear/springsecurity/SpringConfigurationBasic.java
+++ src/main/java/dev/iratherfear/springsecurity/SpringConfigurationBasic.java
@@ -12,8 +12,10 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -32,25 +34,36 @@ public class SpringConfigurationBasic  {
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
+    public UserDetailsService userDetailsService(DataSource dataSource) {
         UserDetails user1 = User.withUsername("test1")
-                                .password("{noop}test")
+                                .password("test")
+                                .passwordEncoder(password -> passwordEncoder().encode(password))
                                 .roles("USER")
                                 .build();
         
         UserDetails user2 = User.withUsername("test2")
-                                .password("{noop}test")
+                                .password("test")
+                                .passwordEncoder(password -> passwordEncoder().encode(password))
                                 .roles("ADMIN")
                                 .build();
+
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.createUser(user1);
+        jdbcUserDetailsManager.createUser(user2);
 
         return new InMemoryUserDetailsManager(user1, user2);
     }
 
-    // @Bean
-    // public DataSource dataSource() {
-    //     return new EmbeddedDatabaseBuilder()
-    //                     .setType(EmbeddedDatabaseType.H2)
-    //                     .addScript(org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
-    //                     .build();
-    // }
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                        .setType(EmbeddedDatabaseType.H2)
+                        .addScript(org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                        .build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
