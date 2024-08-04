992719dce64570edf976f5bcf135f9a41af381ff
iratherfear <irather@fear.mail>
2024-08-04 15:54:43 +0530

    jwt auth done

--- src/main/java/dev/iratherfear/springsecurity/Controller.java
+++ src/main/java/dev/iratherfear/springsecurity/Controller.java
@@ -1,18 +1,35 @@
 package dev.iratherfear.springsecurity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-
+record Todo(String username, String description) {}
 
 @RestController
-@RequiredArgsConstructor
 public class Controller {
     
+    List<Todo> todos = new ArrayList<>(List.of(
+        new Todo("user1", "Description 1"),
+        new Todo("user2", "Description 2")
+    ));
+
     @GetMapping("/")
-    public String getHelloWorld() {
-        return new String("Hello W11orld!!");
+    public String helloWorldMapping() {
+        return new String("Hello World!!");
     }
     
+    @GetMapping("/users/todos")
+    public List<Todo> getMethodName() {
+        return todos;
+    }
+
+    @PostMapping("/users/todos")
+    public void createTodo( @RequestBody Todo todo) {
+        todos.add(todo);
+    }
 }
--- src/main/java/dev/iratherfear/springsecurity/SpringConfigurationJwt.java
+++ src/main/java/dev/iratherfear/springsecurity/SpringConfigurationJwt.java
@@ -1,23 +1,40 @@
 package dev.iratherfear.springsecurity;
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPublicKey;
+import java.util.UUID;
+
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-// @Configuration
-// @EnableWebSecurity
-public class SpringConfigurationBasic  {
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+
+@Configuration
+@EnableWebSecurity
+public class SpringConfigurationJwt  {
     
     @Bean
     public SecurityFilterChain applicationSecurity(HttpSecurity httpSecurity) throws Exception {
@@ -27,6 +44,7 @@ public class SpringConfigurationBasic  {
             .authorizeHttpRequests(requests -> requests.anyRequest().authenticated())
             .httpBasic().and()
             .headers().frameOptions().sameOrigin().and()
+            .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
             .build();
     }
 
@@ -62,5 +80,36 @@ public class SpringConfigurationBasic  {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public KeyPair keyPair() throws Exception {
+        var keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(2048);
+        return keyPairGenerator.genKeyPair();
+    }
+
+    @Bean
+    public RSAKey rsaKey(KeyPair keyPair) {
+        return new RSAKey.Builder((RSAPublicKey) keyPair.getPublic())
+                .privateKey(keyPair.getPrivate())
+                .keyID(UUID.randomUUID().toString())
+                .build();
+    }
+
+    @Bean
+    public JWKSource<SecurityContext> jwkSource(RSAKey rsaKey) {
+        var jwkSet = new JWKSet(rsaKey);
+        return (jwkSelector, context) -> jwkSelector.select(jwkSet);
+    } 
+
+    @Bean
+    public JwtDecoder jwtDecoder(RSAKey rsaKey) throws Exception {
+        return NimbusJwtDecoder.withPublicKey(rsaKey.toRSAPublicKey()).build();
+    }
+
+    @Bean
+    public JwtEncoder jwtEncoder(JWKSource<SecurityContext> jwkSource) {
+        return new NimbusJwtEncoder(jwkSource);
     }
 }
